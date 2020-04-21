a96d57ae070ecfdbab6710f50d78b0a89fb9eae7
femersky <forte201253@gmail.com>
2020-04-21 21:02:04 +0300

    hints filter component

---
+++ angular-client/src/app/shared/hints-filter.component.ts
@@ -0,0 +1,33 @@
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+
+import { fromEvent } from 'rxjs';
+import { distinctUntilChanged, debounceTime } from 'rxjs/operators/';
+
+@Component({
+  selector: 'hts-hints-filter',
+  templateUrl: './hints-filter.component.html',
+  styleUrls: ['./hints-filter.component.css'],
+})
+export class HintsFilterComponent implements OnInit {
+  @ViewChild('filterField', { static: true }) filterField: ElementRef;
+  @Output() filterFieldInput = new EventEmitter<string>();
+
+  constructor() {}
+
+  ngOnInit() {
+    fromEvent(this.filterField.nativeElement, 'input')
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe((_) => {
+        const value = this.filterField.nativeElement.value;
+        console.log('here');
+        this.filterFieldInput.emit(value);
+      });
+  }
+}
