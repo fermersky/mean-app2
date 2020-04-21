9d828f4ebf14c139122264fb571dee0a0b71988f
femersky <forte201253@gmail.com>
2020-04-21 21:02:48 +0300

    hints list component

--- angular-client/src/app/shared/shared.module.ts
+++ angular-client/src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HintsListComponent } from './hints-list.component';
+import { MaterialModule } from '../material/material.module';
+import { HintsFilterComponent } from './hints-filter.component';
 
 @NgModule({
-  declarations: [HintsListComponent],
-  imports: [CommonModule],
-  exports: [HintsListComponent],
+  declarations: [HintsListComponent, HintsFilterComponent],
+  imports: [CommonModule, MaterialModule],
+  exports: [HintsListComponent, HintsFilterComponent],
 })
 export class SharedModule {}
